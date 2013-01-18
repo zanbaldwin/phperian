5fa64a6c7dfe11f393141e48f2f1e52aa49eb32f
Zander Baldwin <mynameiszanders@gmail.com>
2013-01-18 00:37:47 +0000

    Replace the example unit test with one for the Exception class.

--- tests/Nosco/ExceptionTest.php
+++ tests/Nosco/ExceptionTest.php
@@ -7,16 +7,13 @@
     /**
      * Nosco's Library for Experian Web Services
      *
-     * This is a convinience file for bootstrapping the library during testing.
-     * Do not call this file if you are autoloading via Composer.
-     *
      * @package     Nosco
      * @category    Experian
      * @author      Zander Baldwin <mynameiszanders@gmail.com>
      * @license     MIT/X11 <http://j.mp/mit-license>
-     * @link        https://github.com/mynameiszanders/experianwebservice/blob/develop/tests/Nosco/ExampleTest.php
+     * @link        https://github.com/mynameiszanders/experianwebservice/blob/develop/tests/Nosco/ExceptionTest.php
      */
-    class ExampleTest extends TestCase
+    class ExceptionTest extends TestCase
     {
 
         /**
@@ -25,9 +22,11 @@
          * @access public
          * @return void
          */
-        public function testExample()
+        public function testExceptionExtendsCorrectly()
         {
-            $this->assertTrue(true);
+            $exception = new Exception;
+            $this->assertTrue(is_a($exception, '\\Exception'));
+            $this->assertTrue(is_a($exception, '\\Nosco\\Exception'));
         }
 
     }
