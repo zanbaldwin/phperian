c4098b0ef834747cd03903d471d9559f169915a0
Zander Baldwin <mynameiszanders@gmail.com>
2013-01-18 01:18:09 +0000

    Update Request class.
    Updated Request class to look for sub-classes under the namespace of \Nosco\Request\Partial, as that is the class that they will extend.
    Also updated the DocComment to specify that the exception returned is actually \Nosco\Exception.

--- src/Nosco/Request.php
+++ src/Nosco/Request.php
@@ -34,7 +34,7 @@
          * Obviously, if the method does not exist, throw an exception.
          *
          * @access public
-         * @throws Exception
+         * @throws \Nosco\Exception
          * @return object
          */
         public function __call($method, $arguments)
@@ -44,7 +44,7 @@
             // string identifies the class to initiate.
             if(preg_match('/^create([A-Z].*)$/', $method, $matches)) {
                 // Make sure that the class specified in the method exists as a Request sub-class.
-                $class = '\\' . __CLASS__ . '\\' . $matches[1];
+                $class = '\\' . __CLASS__ . '\\Partial\\' . $matches[1];
                 if(class_exists($class)) {
                     // It does? Great! Create a new instance based on the arguments passed and return it.
                     $reflection = new \ReflectionClass($class);
