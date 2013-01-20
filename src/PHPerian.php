6ba09067c5f8c60c38e28defb1d91c6c4f573206
Zander Baldwin <mynameiszanders@gmail.com>
2013-01-20 21:46:24 +0000

    Move autoloader to main package file.
    Move the \PHPerian\TestLoader class to \PHPerian. There is no point in having two files for a job that can be done in one. Also the TestLoader class doesn't actually provide package functionality so is misrepresented in the directory listing.

--- src/PHPerian.php
+++ src/PHPerian.php
@@ -3,8 +3,12 @@
     /**
      * PHPerian: PHP library for Experian's Web Services
      *
-     * This is a convinience file for bootstrapping the library during testing.
-     * Do not call this file if you are autoloading via Composer.
+     * This class provides static methods for loading and autoloading library
+     * classes during testing. The most common use for this class is to simply
+     * call `\PHPerian\TestLoader::registerAutoloader()` before using any library
+     * components.
+     * Please do not use this class directly. If you are not autoloading from
+     * Composer, you should include the file `../PHPerian.php`.
      *
      * @package     PHPerian
      * @category    Library
@@ -12,6 +16,50 @@
      * @license     MIT/X11 <http://j.mp/mit-license>
      * @link        https://github.com/mynameiszanders/phperian/blob/develop/src/PHPerian.php
      */
+    class PHPerian
+    {
 
-    require_once 'PHPerian/TestLoader.php';
-    \PHPerian\TestLoader::registerAutoloader();
+        /**
+         * Load A Library Class
+         *
+         * Perform checks to make sure only local library classes are loaded,
+         * and the class file exists within the library path.
+         *
+         * @static
+         * @access public
+         * @param string $class
+         * @return void
+         */
+        public static function load($class)
+        {
+            if(substr($class, 0, 9) !== 'PHPerian\\') {
+                return;
+            }
+            $library_root = realpath(__DIR__ . DIRECTORY_SEPARATOR);
+            $file = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+            $file = realpath($library_root . DIRECTORY_SEPARATOR . $file);
+            if(substr($file, 0, strlen($library_root)) == $library_root) {
+                if(is_readable($file)) {
+                    include $file;
+                }
+            }
+        }
+
+        /**
+         * Register Autoloader
+         *
+         * Register an autoloader for Nosco's Experian library.
+         *
+         * @static
+         * @access public
+         * @return boolean
+         */
+        public static function registerAutoloader()
+        {
+            return spl_autoload_register(array('PHPerian', 'load'));
+        }
+
+    }
+
+    // Add the PHPerian autoloader method to the PHP's SPL Autoloader.
+    PHPerian::registerAutoloader();
