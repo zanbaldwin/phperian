0b16aebab13eed02a2a1efb33ac998e732ec547d
Zander Baldwin <mynameiszanders@gmail.com>
2013-01-24 04:48:02 +0000

    Add to Alias class.
    Add the class constants that are going to be used through the class.

--- src/PHPerian/Request/Partial/Applicant/Alias.php
+++ src/PHPerian/Request/Partial/Applicant/Alias.php
@@ -19,4 +19,18 @@
      */
     class Alias extends Partial
     {
+
+        // Data length validation.
+        const MAX_CHARS_TITLE       = 10;
+        const MAX_CHARS_FORENAME    = 15;
+        const MAX_CHARS_MIDDLENAME  = 15;
+        const MAX_CHARS_SURNAME     = 30;
+        const MAX_CHARS_SUFFIX      = 10;
+
+        // Field values.
+        const SOURCE_PROPOSAL       = 'P';
+        const SOURCE_EXISTING       = 'E';
+        const SOURCE_TELEPHONE      = 'T';
+        const SOURCE_OTHER          = 'O';
+        
     }
