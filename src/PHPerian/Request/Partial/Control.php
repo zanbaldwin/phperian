531ed7a0d71fdd74c4c1887c87d1693ac07e1955
Zander Baldwin <mynameiszanders@gmail.com>
2013-01-25 00:19:16 +0000

    Added Control class.
    Added the class \PHPerian\Request\Partial\Control for aiding with the generation of the Control XML block.

---
+++ src/PHPerian/Request/Partial/Control.php
@@ -0,0 +1,22 @@
+<?php
+
+    namespace PHPerian\Request\Partial;
+
+    use \PHPerian\Request\Partial as Partial;
+    use \PHPerian\Exception as Exception;
+
+    /**
+     * PHPerian: PHP library for Experian's Web Services
+     *
+     * A class for assisting with the generation of the Control XML block for request SOAP requests to Experian's Web
+     * Services.
+     *
+     * @package     PHPerian
+     * @category    Library
+     * @author      Zander Baldwin <mynameiszanders@gmail.com>
+     * @license     MIT/X11 <http://j.mp/mit-license>
+     * @link        https://github.com/mynameiszanders/phperian/blob/develop/src/PHPerian/Request/Partial/Control.php
+     */
+    class Control extends Partial
+    {
+    }
