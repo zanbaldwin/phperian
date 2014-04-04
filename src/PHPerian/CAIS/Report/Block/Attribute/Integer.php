b3a1c85a4440a9bea5394e5d7762a3fd272f57ac
Zander Baldwin <mynameiszanders@gmail.com>
2014-04-04 17:05:21 +0100

    Update Integer Attribute

--- src/PHPerian/CAIS/Report/Block/Attribute/Integer.php
+++ src/PHPerian/CAIS/Report/Block/Attribute/Integer.php
@@ -6,12 +6,12 @@
     use \PHPerian\CAIS\Interfaces\Attribute as AttributeInterface;
     use \PHPerian\Exceptions;
 
-    class Interger extends Attribute implements AttributeInterface
+    class Integer extends Attribute implements AttributeInterface
     {
 
         public function setValue($value)
         {
-            if(!is_int($value) || !preg_match('/^(0|[1-9]\\d*)$/') || $value < 0) {
+            if(!is_int($value) || !preg_match('/^(0|[1-9]\\d*)$/', $value) || $value < 0) {
                 throw new Exceptions\InvalidDataType('Integer attributes require their value to be a positive integer or the string-representation of one.');
             }
             $this->value = $value;
