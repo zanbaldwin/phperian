c814a3d5a7969f2d2ebf8f794b13eab476b992f6
Zander Baldwin <mynameiszanders@gmail.com>
2014-04-04 16:54:06 +0100

    Update Attribute Abstract and Interface

--- src/PHPerian/CAIS/Interfaces/Attribute.php
+++ src/PHPerian/CAIS/Interfaces/Attribute.php
@@ -9,18 +9,23 @@
         const JUSTIFY_RIGHT = false;
 
         const ATTRIBUTE_NS      = '\\PHPerian\\CAIS\\Report\\Block\\Attribute';
-        const READONLY          = 'ReadOnly';
-        const ALPHA             = 'Alpha';
 
-        public function __construct($start, $end, $name, $value, $justification = self::JUSTIFY_LEFT, $padding = ' ');
+        const ALPHA             = 'Alpha';
+        const ALPHANUMERIC      = 'AlphaNumeric';
+        const BOOLEAN           = 'Boolean';
+        const DATE              = 'Date';
+        const INTEGER           = 'Integer';
+        const READONLY          = 'ReadOnly';
+
+        public function __construct($start, $end, $name, $value, $justification = null/*self::JUSTIFY_LEFT*/, $padding = ' ');
 
         public function getStartByte();
         public function getEndByte();
+        public function getLength();
         public function getName();
         public function getJustification();
         public function getPadding();
 
-        public function getType();
         public function getValue();
         public function setValue($value);
         public function getString();
--- src/PHPerian/CAIS/Report/Block/Attribute.php
+++ src/PHPerian/CAIS/Report/Block/Attribute.php
@@ -1,6 +1,6 @@
 <?php
 
-    namespace PHPerian\CAIS\Report\Block\Body\Record;
+    namespace PHPerian\CAIS\Report\Block;
 
     use \PHPerian\Exceptions;
     use \PHPerian\CAIS\Interfaces\Attribute as AttributeInterface;
@@ -12,7 +12,7 @@
         protected $end;
         protected $name;
         protected $value;
-        protected $justification    = self::JUSTIFY_LEFT;
+        protected $justification    = AttributeInterface::JUSTIFY_LEFT;
         protected $padding          = ' ';
 
         /**
@@ -21,24 +21,35 @@
          * @access public
          * @return void
          */
-        public function __construct($start, $end, $name, $value, $justification = null, $padding = null)
+        public function __construct($start, $end, $name, $defaultValue, $justification = null, $padding = null)
         {
             // Make sure that the start position is an integer above zero. Either in the integer data-type, or a
             // string-representation of one.
             if((!is_int($start) || $start < 0) && (!is_string($start) || !preg_match('/^(0|[1-9]\\d*)$/', $start))) {
                 throw new Exceptions\InvalidArgument('The start position should be a positive integer.');
             }
-            $this->start = (int) $start;
+            // The Experian CAIS documentation specifies the start byte to be one greater than it should be. They do not
+            // used zero-based counting with this. It can get rather annoying.
+            $this->start = (int) $start - 1;
             // Make sure that the end position is an integer above zero. Either in the integer data-type, or a
             // string-representation of one.
             if((!is_int($end) || $end < 0) && (!is_string($end) || !preg_match('/^(0|[1-9]\\d*)$/', $end))) {
                 throw new Exceptions\InvalidArgument('The end position should be a positive integer.');
             }
-            $this->end = (int) $end;
+            // The Experian CAIS documentation specifies the start byte to be one greater than it should be. They do not
+            // used zero-based counting with this. It can get rather annoying.
+            $this->end = (int) $end - 1;
             // The name is not really important, but if it's not a non-empty string then don't bother setting it.
             $this->name = is_string($name) && !empty($name)
                 ? $name
                 : null;
+            // Attempt to set the default value.
+            try {
+                $this->setValue($defaultValue);
+            }
+            catch(Exceptions\Exception $e) {
+                throw new InvalidArgument('The attributes default value could not be set.');
+            }
             // Check that the justification is correct. The constants use the boolean data-type.
             if($justification !== null) {
                 if(!is_bool($justification)) {
@@ -70,7 +81,7 @@
          * Get: End Byte
          *
          * @access public
-         * @return *
+         * @return integer
          */
         public function getEndByte()
         {
@@ -78,10 +89,21 @@
         }
 
         /**
+         * Get: Length
+         *
+         * @access public
+         * @return integer
+         */
+        public function getLength()
+        {
+            return ($this->end - $this->start) + 1;
+        }
+
+        /**
          * Get: Attribute Name
          *
          * @access public
-         * @return *
+         * @return string
          */
         public function getName()
         {
@@ -92,7 +114,7 @@
          * Get: Justification
          *
          * @access public
-         * @return *
+         * @return boolean
          */
         public function getJustification()
         {
@@ -103,7 +125,7 @@
          * Get: Padding
          *
          * @access public
-         * @return *
+         * @return string
          */
         public function getPadding()
         {
@@ -111,21 +133,21 @@
         }
 
         /**
-         * Get: Type
+         * Get: Value
          *
          * @access public
-         * @return *
+         * @return mixed
          */
-        public function getType()
+        public function getValue()
         {
-            return $this->type;
+            return $this->value;
         }
 
         /**
          * Get: Value (String Form)
          *
          * @access public
-         * @return *
+         * @return string
          */
         public function getString()
         {
