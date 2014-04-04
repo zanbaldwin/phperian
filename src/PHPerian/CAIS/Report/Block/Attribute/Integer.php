2342a5fddf910579caebde36fa65f818ab282128
Zander Baldwin <mynameiszanders@gmail.com>
2014-04-04 19:21:41 +0100

    Update Integer Attribute

--- src/PHPerian/CAIS/Report/Block/Attribute/Integer.php
+++ src/PHPerian/CAIS/Report/Block/Attribute/Integer.php
@@ -11,7 +11,7 @@
 
         public function setValue($value)
         {
-            if(!is_int($value) || !preg_match('/^(0|[1-9]\\d*)$/', $value) || $value < 0) {
+            if(!is_int($value) && !preg_match('/^(0|[1-9]\\d*)$/', $value) && $value < 0) {
                 throw new Exceptions\InvalidDataType('Integer attributes require their value to be a positive integer or the string-representation of one.');
             }
             $this->value = $value;
