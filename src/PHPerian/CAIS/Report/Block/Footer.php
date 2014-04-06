c60148f6aec274af4c832cacadbe459047d43210
Zander Baldwin <mynameiszanders@gmail.com>
2014-04-06 17:13:32 +0100

    Add Footer Attribute Definitions

--- src/PHPerian/CAIS/Report/Block/Footer.php
+++ src/PHPerian/CAIS/Report/Block/Footer.php
@@ -8,4 +8,35 @@
 
     class Footer extends Collection
     {
+
+        protected static $attributeDefinitions = array(
+            'trailerIdentifier' => array(
+                'type'          => AttributeInterface::READONLY,
+                'start'         => 1,
+                'end'           => 20,
+                'name'          => 'Trailer Identifier',
+                'default'       => 'HEADER',
+                'justification' => AttributeInterface::JUSTIFY_RIGHT,
+                'padding'       => '9',
+            ),
+            'totalNumberOfRecords' => array(
+                'type'          => AttributeInterface::INTEGER,
+                'start'         => 21,
+                'end'           => 28,
+                'name'          => 'Total Number of Records',
+                'default'       => null,
+                'justification' => AttributeInterface::JUSTIFY_RIGHT,
+                'padding'       => '0'
+            ),
+            'filler' => array(
+                'type'          => AttributeInterface::READONLY,
+                'start'         => 29,
+                'end'           => 530,
+                'name'          => 'Filler',
+                'default'       => null,
+                'justification' => AttributeInterface::JUSTIFY_RIGHT,
+                'padding'       => ' '
+            ),
+        );
+
     }
