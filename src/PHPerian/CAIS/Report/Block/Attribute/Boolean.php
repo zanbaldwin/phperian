83ec6db36eb27a31663a51dfe5a8140983f53c3e
Zander Baldwin <mynameiszanders@gmail.com>
2014-04-09 12:45:51 +0100

    Update Boolean Attribute
    Allow an integer (or string-representation of) to be passed as a boolean value, providing it is zero or one.

--- src/PHPerian/CAIS/Report/Block/Attribute/Boolean.php
+++ src/PHPerian/CAIS/Report/Block/Attribute/Boolean.php
@@ -20,12 +20,18 @@
          */
         public function setValue($value)
         {
-            if(!is_bool($value) && (!is_string($value) || strlen($value) !== 1 || strpos($this->flags, $value) === false)) {
+            if(is_bool($value)) {
+                $this->value = $value;
+            }
+            elseif(preg_match('/^[01]$/', $value)) {
+                $this->value = (bool) $value;
+            }
+            elseif(is_string($value) && strlen($value) === 1 && strpos($this->flags, $value) !== false) {
+                $this->value = !strpos($this->flags, $value);
+            }
+            else {
                 throw new Exceptions\InvalidDataType('Boolean attributes require their values to be of boolean data type, or one of their single character flags.');
             }
-            $this->value = is_string($value)
-                ? !strpos($this->flags, $value)
-                : $value;
         }
 
         /**
